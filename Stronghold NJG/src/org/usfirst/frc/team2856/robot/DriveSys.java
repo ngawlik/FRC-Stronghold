60fb95bf0ce60d85455d035a9aa879c8135aaa73
ngawlik <noahgawlik@gmail.com>
2016-03-03 23:25:29 -0500

    Removed joystick from drive class and added some encoder functions

--- Stronghold NJG/src/org/usfirst/frc/team2856/robot/DriveSys.java
+++ Stronghold NJG/src/org/usfirst/frc/team2856/robot/DriveSys.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team2856.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 public class DriveSys {
@@ -13,11 +12,24 @@ public class DriveSys {
 	public void placeholder() {
 		
 	}
-	
-	public void arcadeDrive(GenericHID stick) {
-		robotDrive.arcadeDrive(stick, true);
+
+	public void arcadeDrive(double moveValue, double rotateValue) {
+		robotDrive.arcadeDrive(moveValue, rotateValue, true);
 	}
-	
+
+	public double encoderGetDistLeft() {
+		return RobotMap.DRIVE_ENCODER_LEFT.getDistance();
+	}
+
+	public double encoderGetDistRight() {
+		return RobotMap.DRIVE_ENCODER_RIGHT.getDistance();
+	}
+
+	public void encoderReset() {
+		RobotMap.DRIVE_ENCODER_LEFT.reset();
+		RobotMap.DRIVE_ENCODER_RIGHT.reset();
+	}
+
 	public void stop() {
 		robotDrive.stopMotor();
 	}
--- Stronghold NJG/src/org/usfirst/frc/team2856/robot/Teleop.java
+++ Stronghold NJG/src/org/usfirst/frc/team2856/robot/Teleop.java
@@ -1,13 +1,18 @@
 package org.usfirst.frc.team2856.robot;
 
+//import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class Teleop {
+//	private DriverStation ds;
 	private DriveSys drive;
 	private IntakeSys intake;
 	private Joystick joystick;
+	private boolean button11PrevValue;
+	private boolean button12PrevValue;
 
 	public Teleop(DriveSys driveObj, IntakeSys intakeObj) {
+//		ds = DriverStation.getInstance();
 		drive = driveObj;
 		intake = intakeObj;
 		joystick = new Joystick(0);
@@ -18,31 +23,42 @@ public class Teleop {
 	}
 
 	public void periodic() {
-		drive.arcadeDrive(joystick);
+		drive.arcadeDrive(joystick.getY(), joystick.getX());
 		
 		double effort = 0;
-        if (joystick.getRawButton(2))
-        {
-        	if (joystick.getRawButton(1))
-        	{
-        		effort = -1.0;
-        	}
-        	else
-        	{
-        		effort = -0.2;
-        	}
-        }
-        else if (joystick.getRawButton(3))
-        {
-        	if (joystick.getRawButton(1))
-        	{
-        		effort = 1.0;
-        	}
-        	else
-        	{
-        		effort = 0.2;
-        	}
-        }
+		if (joystick.getRawButton(5))
+		{
+			effort = 0.2;
+		}
+		else if (joystick.getRawButton(3))
+		{
+			effort = 1.0;
+		}
+		else if (joystick.getRawButton(6))
+		{
+			effort = -0.2;
+		}
+		else if (joystick.getRawButton(4))
+		{
+			effort = -1.0;
+		}
         intake.setEffort(effort);
+        
+    	boolean button11Value = joystick.getRawButton(11);
+    	if (button11Value && !button11PrevValue)
+    	{
+    		double left = drive.encoderGetDistLeft();
+    		double right = drive.encoderGetDistRight();
+    		System.out.printf("Drive Encoder Dist L:%.2f R:%.2f\n", left, right);
+    	}
+    	button11PrevValue = button11Value;
+    	
+    	boolean button12Value = joystick.getRawButton(12);
+    	if (button12Value && !button12PrevValue)
+    	{
+    		drive.encoderReset();
+    		System.out.println("Drive Encoder Reset");
+    	}
+    	button12PrevValue = button12Value;
 	}
 }
